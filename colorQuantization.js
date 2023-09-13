0399c7449daf9217f215bb35870a01ee9acf74d1
Maggie Negm <maggienegm02@gmail.com>
2023-09-13 00:00:51 -0400

    feat: sort rgbValues and return final colors

--- colorQuantization.js
+++ colorQuantization.js
@@ -32,21 +32,22 @@
             // Organize Array to into RGB format
             let buildRgb = (imgData) => {
             
-            for (let i = 0; i < imgData.length; i += 4) {
-                let rgb = {
-                r: imgData[i],
-                g: imgData[i + 1],
-                b: imgData[i + 2],
-                };
-                rgbValues.push(rgb);
-            }
-            return rgbValues;
-        };
-         console.log(buildRgb(imgData)); 
+                for (let i = 0; i < imgData.length; i += 4) {
+                    let rgb = {
+                    r: imgData[i],
+                    g: imgData[i + 1],
+                    b: imgData[i + 2],
+                    };
+                    rgbValues.push(rgb);
+                }
+                return rgbValues;
+            };
+        console.log(buildRgb(imgData)); 
 
         // Color Quantization using the Median Cut algorithm 
 
         let findBiggestColorRange = (rgbValues) => {
+            // TODO: change let variables below to const
             let rMin = 255;
             let gMin = 255;
             let bMin = 255;
@@ -79,9 +80,42 @@
                 return "b";
             }
             };
-           console.log(findBiggestColorRange(rgbValues));
+        console.log(findBiggestColorRange(rgbValues));
 
-        }
+        const quantization = (rgbValues, depth) => {
+            const MAX_DEPTH = 5;
 
+            if (depth === MAX_DEPTH || rgbValues.length === 0) {
+                const color = rgbValues.reduce((pixel1, pixel2) => 
+                    {
+                        pixel1.r += pixel2.r;
+                        pixel1.g += pixel2.g;
+                        pixel1.b += pixel2.b;
+                        return pixel1;
+                    },
+                    {
+                        r: 0,
+                        g: 0,
+                        b: 0,
+                    }
+                );
+                color.r = Math.round(color.r / rgbValues.length);
+                color.g = Math.round(color.g / rgbValues.length);
+                color.b = Math.round(color.b / rgbValues.length);
+                return [color];
+            }
+
+            const colorToSortBy = findBiggestColorRange(rgbValues);
+            rgbValues.sort((pixel1, pixel2) => {
+                return pixel1[colorToSortBy] - pixel2[colorToSortBy];
+            });
+            const mid = rgbValues.length / 2;
+            return [
+                ...quantization(rgbValues.slice(0, mid), depth + 1),
+                ...quantization(rgbValues.slice(mid + 1), depth + 1),
+            ];
         };
-    grabImg();
+        console.log('final colors:', quantization(rgbValues, 2));
+    }
+};
+grabImg();
