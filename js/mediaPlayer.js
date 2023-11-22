27528648141338fdca79408fc911e4c87dbc4cd7
Maggie Negm <maggienegm02@gmail.com>
2023-11-22 16:32:22 -0500

    feat: improve SEO with meta tags and alt text for imgs

--- js/mediaPlayer.js
+++ js/mediaPlayer.js
@@ -1,45 +1,52 @@
 const soundData = [
     {
         imgUrl: 'images/soft-rain.jpg',
-        imgDescription: 'Cozy Downpour',
+        imgDescription: 'A scene of light rain during the day in a residential neighborhood and a distant figure ahead is walking with an umbrella.',
         soundUrl: 'https://audio.jukehost.co.uk/xcb125pumHo2Lw4S9bEzfuSsWTToAmva',
         soundTitle: 'Soft Rain',
+        soundSubtitle: 'Cozy Downpour',
     },
     {
         imgUrl: 'images/rain-and-thunder.jpg',
-        imgDescription: 'Embracing the Storm',
+        imgDescription: 'A close up scene of a window with raindrops on the window pane overlooking a dark and gloomy sky with dense clouds.',
         soundUrl: 'https://audio.jukehost.co.uk/OFAwwnonDnu0eNi6wbAFEF7C0ZHNTTxeh',
         soundTitle: 'Rain & Thunder',
+        soundSubtitle: 'Embracing the Storm',
     },
     {
         imgUrl: 'images/calm-waves.jpg',
-        imgDescription: 'A Day by the Ocean',
+        imgDescription: 'A closeup scene of the beach and calm beach waves during the day.',
         soundUrl: 'https://audio.jukehost.co.uk/OgulFZmCpzhomsAsnaWAIJ02BtFnP3gI',
         soundTitle: 'Calm Waves',
+        soundSubtitle: 'A Day by the Ocean',
     },
     {
         imgUrl: 'images/campfire.jpg',
-        imgDescription: 'Finding Solace by the Campfire',
+        imgDescription: 'A closeup of a campfire burning during the day surrounded by dense forest trees in the background.',
         soundUrl: 'https://audio.jukehost.co.uk/VQHB0H4Uz5SnJfnznuvGe7BWyLCBlQ07',
         soundTitle: 'Campfire',
+        soundSubtitle: 'Finding Solace by the Campfire',
     },
     {
         imgUrl: 'images/forest.jpg',
-        imgDescription: 'Symphony of the Woods',
+        imgDescription: 'A scene of an enchanting forest with sunlight peeking through the trees and fluffy green and yellow grass.',
         soundUrl: 'https://audio.jukehost.co.uk/28zsHlunydp6wUgQahr8aTXcFyNa0dch',
         soundTitle: 'Forest',
+        soundSubtitle: 'Symphony of the Woods',
     },
     {
         imgUrl: 'images/birds.jpg',
-        imgDescription: 'Morning Serenade: Birds at Play',
+        imgDescription: 'A closeup of a bird in mid-flight with its wings open eating from a group of hanging berries. The background is pink.',
         soundUrl: 'https://audio.jukehost.co.uk/iHA1C4FAgkeCxt9DvGmBjgllP9ALocFf',
         soundTitle: 'Birds Chirping',
+        soundSubtitle: 'Morning Serenade: Birds at Play',
     },
     {
         imgUrl: 'images/snowfall.jpg',
-        imgDescription: 'Snowed In',
+        imgDescription: 'In the midst of a snowstorm stands a long and blue cylinder shaped house with three floors surrounded by inches of snow and forest trees.',
         soundUrl: 'https://audio.jukehost.co.uk/aRFcxDzq9wlEnYwx2GBVqquYob7ekWw7',
         soundTitle: 'Snowfall',
+        soundSubtitle: 'Snowed In',
     },
 ];
 let isMenuOpen = false;
@@ -49,10 +56,10 @@ function generateSoundsList(soundsListElementId) {
     if (soundsList && !soundsList.innerHTML) {
         for (let i = 0; i < soundData.length; i++) {
             const li = `<li data-sound-id=${i} class="list-sound-item" onclick="playSelectedSound(${i})">
-                            <img class="list-sound-image box-shadow" src="${soundData[i].imgUrl}">
+                            <img class="list-sound-image box-shadow" src="${soundData[i].imgUrl}" alt="${soundData[i].imgDescription}">
                             <div>
                                 <p class="margin-none padding-none">${soundData[i].soundTitle}</p>
-                                <p class="list-image-description text-subtitle margin-none text-opacity">${soundData[i].imgDescription}</p>
+                                <p class="list-sound-subtitle text-subtitle margin-none text-opacity">${soundData[i].soundSubtitle}</p>
                             </div>
                         </li>`;
             soundsList.innerHTML += li;
@@ -115,7 +122,7 @@ function updateSoundInfo(newSoundId) {
     document.querySelector('.sound-image').src = soundData[newSoundId].imgUrl;
     document.querySelector('.sound-image').alt = soundData[newSoundId].imgDescription;
     document.querySelector('.sound-title').innerHTML = soundData[newSoundId].soundTitle;
-    document.querySelector('.image-description').innerHTML = soundData[newSoundId].imgDescription;
+    document.querySelector('.sound-subtitle').innerHTML = soundData[newSoundId].soundSubtitle;
     document.querySelector('#sound-audio').src = soundData[newSoundId].soundUrl;
     document.querySelector('#sound-audio').dataset.soundId = newSoundId;
 }
